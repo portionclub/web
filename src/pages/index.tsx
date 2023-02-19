7b723b77ef70e32e9c0d074110d7a62333da5ea5
Vance Ingalls <vanceingalls@gmail.com>
2023-02-19 15:32:53 -0800

    feat: homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-import Layout from '../components/layout/Layout';
-import Seo from '../components/Seo';
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
 
 /**
  * SVGR Support
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
           800: 'rgb(var(--tw-color-primary-800) / <alpha-value>)',
           900: 'rgb(var(--tw-color-primary-900) / <alpha-value>)',
         },
-        dark: '#000',
+        dark: '#171819',
         highlight: '#BAEC68',
       },
       keyframes: {
