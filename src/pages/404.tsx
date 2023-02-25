32b0b5f008f4f93d006c45df08692af80c535a7c
Vance Ingalls <vanceingalls@gmail.com>
2023-02-24 21:58:31 -0800

    chore: fix 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,12 @@
+import Link from 'next/link';
 import * as React from 'react';
 import { RiAlarmWarningFill } from 'react-icons/ri';
 
-import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
 import Seo from '@/components/Seo';
 
 export default function NotFoundPage() {
   return (
-    <Layout>
+    <div>
       <Seo templateTitle='Not Found' />
 
       <main>
@@ -18,12 +17,12 @@ export default function NotFoundPage() {
               className='drop-shadow-glow animate-flicker text-red-500'
             />
             <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
-            <ArrowLink className='mt-4 md:text-lg' href='/'>
+            <Link className='mt-4 md:text-lg' href='/'>
               Back to Home
-            </ArrowLink>
+            </Link>
           </div>
         </section>
       </main>
-    </Layout>
+    </div>
   );
 }
