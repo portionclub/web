93f5759aea4de1547ef111a11758a31dc10e090b
Vance Ingalls <vanceingalls@gmail.com>
2023-02-24 21:55:23 -0800

    chore: fix test; change key name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,7 +21,7 @@ import '@rainbow-me/rainbowkit/styles.css';
 const { chains, provider } = configureChains(
   [mainnet, goerli],
   [
-    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY || '' }),
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_TOKEN || '' }),
     publicProvider(),
   ]
 );
