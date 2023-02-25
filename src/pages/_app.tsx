12e312ff2a82eae1884575258699196ccbc9bbda
Vance Ingalls <vanceingalls@gmail.com>
2023-02-24 21:20:48 -0800

    feat: embedded auction

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import {
+  getDefaultWallets,
+  lightTheme,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { NFTFetchConfiguration } from '@zoralabs/nft-hooks/dist/context/NFTFetchContext';
+import { ZDKFetchStrategy } from '@zoralabs/nft-hooks/dist/strategies';
 import { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 import React from 'react';
+import { SWRConfig } from 'swr';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { arbitrum, mainnet, optimism, polygon } from 'wagmi/chains';
+import { goerli, mainnet } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
@@ -11,8 +19,11 @@ import '@/styles/colors.css';
 import '@rainbow-me/rainbowkit/styles.css';
 
 const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID || '' }), publicProvider()]
+  [mainnet, goerli],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY || '' }),
+    publicProvider(),
+  ]
 );
 
 const { connectors } = getDefaultWallets({
@@ -26,11 +37,40 @@ const wagmiClient = createClient({
   provider,
 });
 
+export const strategy = new ZDKFetchStrategy(
+  '1',
+  'https://api.zora.co/graphql'
+);
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
+      <RainbowKitProvider
+        chains={chains}
+        coolMode
+        theme={lightTheme({
+          accentColor: 'black',
+          borderRadius: 'large',
+        })}
+      >
+        <NFTFetchConfiguration networkId='1' strategy={strategy}>
+          <SWRConfig
+            value={{
+              fetcher: (resource, init) =>
+                fetch(resource, init).then((res) => res.json()),
+            }}
+          >
+            <NextNProgress
+              color='#ff89de'
+              startPosition={0.125}
+              stopDelayMs={200}
+              height={2}
+              showOnShallow={true}
+              options={{ showSpinner: false }}
+            />
+            <Component {...pageProps} />
+          </SWRConfig>
+        </NFTFetchConfiguration>
       </RainbowKitProvider>
     </WagmiConfig>
   );
---
+++ src/utils/shortenAddress.ts
@@ -0,0 +1,16 @@
+import { utils } from 'ethers';
+
+export function shortenAddress(address?: string, chars = 4): string {
+  if (!address) {
+    return '';
+  }
+
+  const parsed = utils.getAddress(address);
+
+  if (!parsed) {
+    // eslint-disable-next-line no-console
+    console.error(`Invalid 'address' parameter '${address}'.`);
+    return '';
+  }
+  return `${parsed.substring(0, chars + 2)}...${parsed.substring(42 - chars)}`;
+}
